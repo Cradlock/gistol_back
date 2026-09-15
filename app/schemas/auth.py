df65e4818ae3f2568b26137338ce0fed72fe6977
cradlock <pefig8@gmail.com>
2026-09-15 23:57:25 +0600

    big update 6x

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -51,7 +51,8 @@ class TelegramAuthResponse(BaseModel):
 
 # Refres logic 
 class RefreshTokenResponse(BaseModel):
-    access_token: str 
+    access_token: str
+    refresh_token: str 
 
 class RefreshTokenRequest(BaseModel):
     refresh_token: str 
